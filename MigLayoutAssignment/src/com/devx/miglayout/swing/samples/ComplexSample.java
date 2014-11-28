5ceb70742efdb729415fa0e6d6cd57d35cbc56a9
jamesgleeson <james.gti.gleeson@gmail.com>
2014-11-28 14:17:52 +0000

    Update ComplexSample.java
    Changes  made by James to add drop down menus to the "Worming Dose", "Age" fields and resised the Worming Dose label

--- MigLayoutAssignment/src/com/devx/miglayout/swing/samples/ComplexSample.java
+++ MigLayoutAssignment/src/com/devx/miglayout/swing/samples/ComplexSample.java
@@ -42,7 +42,7 @@ public class ComplexSample extends AbstractSampleFrame {
 	private JPanel getInformationPanel() {
 		JPanel panel = getPanel("Report Generator");
 		panel.setLayout(new MigLayout("wrap 3","[] 16 [grow] []"));
-		panel.add(new JLabel("Animal Type"),"right");
+		panel.add(new JLabel("Species"),"right");
 		panel.add(new JTextField("Horse"),"growx");
 		panel.add(new JCheckBox("In shelter",true));
 		panel.add(new JLabel("From"),"right");
@@ -59,6 +59,139 @@ public class ComplexSample extends AbstractSampleFrame {
 		return panel;
 	}
 	
+	private JPanel getClientPanel() {
+		JPanel panel = getPanel("Enter in Animal Record");
+		JComboBox<String> ages = new JComboBox<String>();
+		ages.addItem("1");
+		ages.addItem("2");
+		ages.addItem("3");
+		ages.addItem("4");
+		ages.addItem("5");
+		ages.addItem(">5");
+		panel.setLayout(new MigLayout("wrap 2","[] 18 []"));
+		
+		panel.add(new JLabel("Animal Type:"),"right");
+		//panel.add(new JLabel("Donkey"));
+		panel.add(new JTextField("Donkey"),"growx");
+	
+		
+		panel.add(new JLabel("Animal name:"),"right");
+		panel.add(new JTextField("Jack Donk"),"growx");
+		
+		panel.add(new JLabel("Vaccination Date:"),"right");
+		panel.add(new JTextField("January 6th 2014"),"growx");
+		
+		panel.add(new JLabel("Vet:"),"right");
+		panel.add(new JTextField("John Steer"),"growx");
+		
+		panel.add(new JLabel("Registration #:"),"right");
+		panel.add(new JTextField("#4534-AAC-324"),"growx");
+		
+		panel.add(new JLabel("Age"),"right");
+		panel.add(ages,"growx");
+		//panel.add(new JTextField("2"),"growx");
+		
+		panel.add(new JLabel("Micorchip Number:"),"right");
+		panel.add(new JTextField("#9860000023456"),"growx");
+		
+		panel.add(getImageButton("images/list-add.png", "Enter into DB"));
+		panel.add(getImageButton("images/edit-redo.png", "Update DB"),"sg 1");
+		panel.add(getImageButton("images/list-remove.png", "Delete Entry"),"sg 1");
+		panel.add(getImageButton("images/edit-redo.png", "Query DB"),"sg 1");
+		
+		return panel;
+		
+	}
+	
+	private JPanel getAdditionalInformationPanel() {
+		JPanel panel = getPanel("Additional information");
+		panel.setLayout(new MigLayout("wrap 2","[] 16 [grow][] []"));
+		
+		JComboBox<String> wormingDose = new JComboBox<String>();
+		wormingDose.addItem("1 Packet");
+		wormingDose.addItem("2 Packets");
+		wormingDose.addItem("3 Packets");
+		wormingDose.addItem("4 Packets");
+		wormingDose.addItem("5 Packets");
+		wormingDose.addItem(">5 Packets");
+		
+		JComboBox<String> homing = new JComboBox<String>();
+		homing.addItem("Yes");
+		homing.addItem("No");
+		
+		panel.add(new JLabel("Worming dose:"),"right");
+		panel.add(wormingDose,"growx");
+		
+		//panel.add(new JButton("test"));
+		
+		panel.add(new JLabel("Worming Date:"),"right");
+		panel.add(new JTextField("01-01-2014"),"growx");
+		
+		panel.add(new JLabel("Wormed by:"),"right");
+		panel.add(new JTextField("Bad Mama"),"growx");
+
+		panel.add(new JLabel("Area found"),"right");
+		panel.add(new JTextField("Co Clare"),"growx");
+
+		panel.add(new JLabel("Handed in by "),"right");
+		panel.add(new JTextField("Clare Co Co"),"growx");
+
+		panel.add(new JLabel("Whereabouts"),"right");
+		panel.add(new JTextField("In foster care"),"growx");
+
+		panel.add(new JLabel("Available for homing"),"right");
+		panel.add(homing,"growx");
+		
+		panel.add(getImageButton("images/edit-redo.png","Edit"),"growx");
+		panel.add(new JSeparator(),"growx");
+		return panel;
+	}
+	
+	private JPanel getNotesPanel() {
+		JPanel panel = getPanel("NOTES");
+		panel.setLayout(new MigLayout("wrap 4","[grow] 16 [] 32 [] []","[grow,:100:] [] [] []"));
+		
+		panel.add(getTable(""),
+				"grow, span 3");
+		panel.add(getImageButton("images/list-add.png", "Add"),"split 3, flowy, sg 1, top");
+		panel.add(getImageButton("images/edit-redo.png", "Edit"),"sg 1");
+		panel.add(getImageButton("images/list-remove.png", "Delete"),"sg 1, wrap");
+		
+		return panel;
+	}
+		
+	private JPanel getButtonsPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new MigLayout("","[] [grow, right]"));
+		
+		panel.add(new JButton("Export"),"sg 1");
+		panel.add(getImageButton("images/document-save.png", "Save"),"split 2, sg 1");
+		panel.add(getImageButton("images/system-log-out.png", "Cancel"),"sg 1");
+		
+		return panel;
+	}
+	
+	private JPanel getPanel(String title) {
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createTitledBorder(title));
+		return panel;
+	}
+	
+	private JButton getImageButton(String imagePath, String text) {
+		java.net.URL imgURL = getClass().getResource(imagePath);
+		//JButton button = new JButton(text,new ImageIcon(imgURL));
+		return button;
+	}
+	
+	private JScrollPane getTable(Object...columns) {
+		JTable table = new JTable(new Object[0][0], columns);
+		JScrollPane scrollPane = new JScrollPane(table);
+		table.setFillsViewportHeight(true);
+		return scrollPane;
+	}
+	
+
+}
 	private JPanel getClientPanel() {
 		JPanel panel = getPanel("Enter in Animal Record");
 		panel.setLayout(new MigLayout("wrap 2","[] 18 []"));
